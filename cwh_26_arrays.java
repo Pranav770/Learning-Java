34f977a0d39b973fd19d22bd214d82dfdf027a74
Pranav770 <pranavpandey2950@gmail.com>
2024-09-24 18:20:27 +0530

    Update cwh_26_arrays.java 1

--- cwh_26_arrays.java
+++ cwh_26_arrays.java
@@ -1,9 +1,24 @@
 public class cwh_26_arrays {
-    public static void main(String[]args){
-        /*Classroom of 500 students-You have to store maeks of these 500 students
-        You have 2 options:
-        1.Create 500 variables
-        2.Use Arrays(recommended)*/
-        int[] marks = new int[5];
+    public static void main(String[] args) {
+        //Classroom of 500 students - You have to store marks of these 500 students
+        //You have 2 options:
+        //1 . Create 500 variables
+        //2. use Arrays(recommended)
+        // There are three main ways to create arrays in java
+        //1, Declaration and memory allocation
+//        int [] marks = new int[5];
+        //2. Declaration and the memory allocation
+//        int [] marks;
+//        marks=new int[5];
+//        marks[0]=  100;
+//        marks[1]=  60;
+//        marks[2]=  70;
+//        marks[3]=  90;
+//        marks[4]=  86;
+//        2. Declaration , memory allocation and  initialization together
+        int [] marks ={98,45,79,99,80};
+
+//        marks[5]=  96; -throws error
+        System.out.println(marks[4]);
     }
 }
